32fa1afa612f39a092de3ad30143fc3271506bb9
Fuad Osman <1fuadosman@gmail.com>
2019-02-13 19:38:49 +0000

    wip

---
+++ src/KeywordsaAndExpressions.java
@@ -0,0 +1,34 @@
+public class KeywordsaAndExpressions {
+
+    public static void main(String[] agrs){
+        //Expressions - are variables/values/operators
+        //a mile is equal to 1.609334 to kilometers
+        double kilometers = (1.609334 * 100);
+        int highScore=50;
+
+        if(highScore==50) {
+            System.out.println("This is an expression");
+        }
+
+        //Challenge
+        //In the following code that i will type below, write downd
+        //what parts of the code are expressinos
+
+        int score = 100;
+        if(score>90) {
+            System.out.println("You got the high score!");
+            score=0;
+        }
+
+        //The expressions above are =,100, sout, the string in sout
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Sec4Lec25StatementsWhileSpaceAndIndenting.java
@@ -0,0 +1,20 @@
+public class Sec4Lec25StatementsWhileSpaceAndIndenting {
+
+    public static void main(String[] agrs){
+
+        int myVariable =50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                "still" +
+                " more");
+
+        int anotherVariable= 50;
+
+        //indenting is just pushing the code forward so its more readable
+
+
+    }
+}
