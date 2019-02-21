5786bdd1913eb35637f4894187fbc7669e1be1b4
Fuad Osman <fuadosman@Fuads-iMac.local>
2019-02-21 17:18:06 +0000

    wip

---
+++ src/Sec4Lec28Methods.java
@@ -0,0 +1,26 @@
+public class Sec4Lec28Methods {
+
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        calculateScore(gameOver,score,levelCompleted,bonus);
+
+        calculateScore(true,10000,8,200);
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if(gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is " + finalScore);
+        }
+
+    }
+
+
+}
