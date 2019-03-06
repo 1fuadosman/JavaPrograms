8da8696a54966d9cbd62271db21a50a9e7403b05
Fuad Osman <fuadosman@ip-172-28-69-94.eu-west-1.compute.internal>
2019-03-06 17:19:50 +0000

    wip

--- src/Sec5Lec45ForStatement.java
+++ src/Sec5Lec45ForStatement.java
@@ -8,20 +8,51 @@ public class Sec5Lec45ForStatement {
         and print the result to the console window.
          */
 
-        for(int i=2; i<=8; i++){
-            System.out.println("10,000 at " +  i + "% " +  "interest = " + String.format("%.2f",calculateInterest(10000, i)));
+//        for(int i=2; i<=8; i++){
+//            System.out.println("10,000 at " +  i + "% " +  "interest = " + String.format("%.2f",calculateInterest(10000, i)));
+//        }
+//
+//        System.out.println("***************************");
+//        //reverse
+//
+//        for(int i=8; i>=2; i--){
+//            System.out.println("10,000 at " +  i + "% " +  "interest = " + String.format("%.2f",calculateInterest(10000, i)));
+//        }
+        int count =0;
+        for(int i=1; i<10; i++) {
+            if(isPrime(i)) {
+                System.out.println(i + " is a prime number");
+                count++;
+                if(count==3){
+                    System.out.println("Exiting loop");
+                    break;
+                }
+            }else {
+                System.out.println(i + " is not a prime number");
+            }
         }
 
-        System.out.println("***************************");
-        //reverse
 
-        for(int i=8; i>=2; i--){
-            System.out.println("10,000 at " +  i + "% " +  "interest = " + String.format("%.2f",calculateInterest(10000, i)));
-        }
+
 
     }
 
     public static double calculateInterest(double amount, double interestRate){
           return (amount * (interestRate / 100));
     }
+
+    public static boolean isPrime(int n) {
+        if(n==1) {
+            return false;
+        }
+
+        for(int i=2; i< n/2; i++){
+            if(n % i ==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
